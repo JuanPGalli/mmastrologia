7dea891aa33e995239deda287ec32e80a25fba4a
JuanPGalli <jpgallir@gmail.com>
2025-05-21 21:35:21 -0300

    add createProductHandler

--- api/src/handlers/productHandlers.ts
+++ api/src/handlers/productHandlers.ts
@@ -18,6 +18,18 @@ export const getProductHandler: RequestHandler = async (req, res) => {
     }
   }
 };
-export const createProductHandler: RequestHandler = async () => {};
+export const createProductHandler: RequestHandler = async (req, res) => {
+  const { name, description, price, imageUrl, category, stock } = req.body;
+  try {
+    await createProduct(name, description, price, imageUrl, category, stock);
+    res.status(200).json(`The product ${name} was successfully created`);
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      res.status(400).json({ error: error.message });
+    } else {
+      res.status(400).json({ error: "Unknown error" });
+    }
+  }
+};
 export const editProductHandler: RequestHandler = async () => {};
 export const deleteProductHandler: RequestHandler = async () => {};
