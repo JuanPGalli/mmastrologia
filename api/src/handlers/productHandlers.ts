e44fd55aebaf5ac6872c55ffc39c5e433e4161e5
JuanPGalli <jpgallir@gmail.com>
2025-05-21 21:13:06 -0300

    managing error safely

--- api/src/handlers/productHandlers.ts
+++ api/src/handlers/productHandlers.ts
@@ -10,8 +10,12 @@ import { RequestHandler } from "express";
 export const getProductHandler: RequestHandler = async (req, res) => {
   try {
     res.status(200).json("Hello World!");
-  } catch (error: any) {
-    res.status(400).json({ error: error.message });
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      res.status(400).json({ error: error.message });
+    } else {
+      res.status(400).json({ error: "Unknown error" });
+    }
   }
 };
 export const createProductHandler: RequestHandler = async () => {};
