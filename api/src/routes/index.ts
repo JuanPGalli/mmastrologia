be97522bcca5e84e04f1ea8925cb150c40fa11eb
JuanPGalli <jpgallir@gmail.com>
2025-05-22 23:54:42 -0300

    /user: add route to index

--- api/src/routes/index.ts
+++ api/src/routes/index.ts
@@ -1,6 +1,9 @@
 import { Router } from "express";
 import productRouter from "./productRouter";
+//import userRouter from "./userRouter";
 
 const router = Router();
 
-router.use('/product', productRouter);
+router.use("/product", productRouter);
+
+router.use("/user" /* userRouter */);
