b9f4516549df49d450ab4b933c05475caa2e9382
Juan Pablo Galli <jpgallir@gmail.com>
2025-12-29 16:54:00 -0300

    add landing, home, about, contact, detail routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,23 @@
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Views/Home/Home';
+import Detail from './Views/Detail/Detail';
+import Landing from './Views/Landing/Landing';
+import About from './Views/About/About';
+import Contact from './Views/Contact/Contact';
 
 function App() {
-  return <div>APP</div>;
+  return (
+    <div className='App'>
+      <Routes>
+        <Route path={'/'} element={<Landing />} />
+        <Route path={'/home'} element={<Home />} />
+        <Route path={'/detail:id'} element={<Detail />} />
+        <Route path={'/about'} element={<About />} />
+        <Route path={'/contact'} element={<Contact />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ client/src/Views/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className='Home'>
+      <h1>Esto es la aplicación MM Astrology</h1>
+    </div>
+  );
+};
+
+export default Home;
