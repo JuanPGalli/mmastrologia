130bf974f8fff31907747dc8020c718e0ee06be8
Juan Pablo Galli <jpgallir@gmail.com>
2025-12-31 20:10:40 -0300

    add components file card, cards and navbar

---
+++ client/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav>
+      <a href='/'>Inicio</a>
+      <a href='/'>Consultas</a>
+      <a href='/'>Sobre Mi</a>
+      <a href='/'>Contacto</a>
+      <a href='/'>Log In</a>
+    </nav>
+  );
+};
+
+export default Navbar;
