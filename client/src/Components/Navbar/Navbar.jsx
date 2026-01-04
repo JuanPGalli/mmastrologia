c00d028d6a3794c88b8f10023dba8cfeecb94261
Juan Pablo Galli <jpgallir@gmail.com>
2026-01-04 01:16:17 -0300

    fix: SEO add <ul/> and <li/> to navbar

--- client/src/Components/Navbar/Navbar.jsx
+++ client/src/Components/Navbar/Navbar.jsx
@@ -1,13 +1,34 @@
-import React from 'react';
+//import React, { useState } from 'react';
+const logoNav = '/logo_mmgastrologia.jpeg';
 
 const Navbar = () => {
+  //const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <nav>
-      <a href='/'>Inicio</a>
-      <a href='/'>Consultas</a>
-      <a href='/'>Sobre Mi</a>
-      <a href='/'>Contacto</a>
-      <a href='/'>Log In</a>
+    <nav className='fixed w-full border-b bg-white shadow'>
+      <div className='mx-auto flex max-w-7xl items-center justify-between px-4 py-3'>
+        {/* Logo */}
+        <a href='/' aria-label='Inicio'>
+          <img src={logoNav} alt='logo_navbar' className='h-10 w-auto' />
+        </a>
+        <ul className='flex items-center justify-between'>
+          <li>
+            <a href='/'>Inicio</a>
+          </li>
+          <li>
+            <a href='/services'>Consultas</a>
+          </li>
+          <li>
+            <a href='/about'>Sobre Mi</a>
+          </li>
+          <li>
+            <a href='/contact'>Contacto</a>
+          </li>
+          <li>
+            <a href='/login'>Log In</a>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 };
