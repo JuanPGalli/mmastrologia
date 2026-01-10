7b34608adf91a1fd5226fa944665384ad17baf20
Juan Pablo Galli <jpgallir@gmail.com>
2026-01-10 20:13:55 -0300

    fix: center text globally and navbar purple style when scrolling

--- client/src/Components/Navbar/Navbar.jsx
+++ client/src/Components/Navbar/Navbar.jsx
@@ -1,13 +1,30 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 const logoNav = '/logo_transparente.png';
 import { FaBars, FaTimes } from 'react-icons/fa';
 import './Navbar.css';
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (scrollY > 0) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, [scrolled]);
 
   return (
-    <nav className='fixed w-full border-b shadow'>
+    <nav
+      className={`fixed top-0 left-0 w-full z-50 transition-colors duration-300 ${
+        scrolled ? 'bg-[rgb(147,116,192)]' : 'bg-transparent'
+      } `}
+    >
       <div className='mx-auto flex max-w-7xl items-center justify-between px-4 py-3'>
         {/* Logo */}
         <a href='/' aria-label='Inicio'>
